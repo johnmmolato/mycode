34074fe921f95d4f22b89a00c21269c9a19ef917
johnmmolato <student@bchd.a1d604ec-0523-46d2-9730-6858bbdfba35>
2022-09-28 22:11:59 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""JMolato
+   Optional challenge"""
+
+def main():
+    animallist = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+    print(animallist)
+
+main()
+
