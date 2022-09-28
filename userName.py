a2a839cc6915b7c6e0bf03024ad4eefa78e4f1b9
johnmmolato <student@bchd.a1d604ec-0523-46d2-9730-6858bbdfba35>
2022-09-28 20:46:28 +0000

    mixed lists

---
+++ userName.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    userName = input("What is your name?")
+    dayOfWeek = input("What day is it?")
+
+    print("Hello," +  userName + "! Happy " +  dayOfWeek + " !")
+
+main()
