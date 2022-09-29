75edfd87f1ee40a5eea3a72248de0e727b208b37
johnmmolato <student@bchd.a1d604ec-0523-46d2-9730-6858bbdfba35>
2022-09-29 19:34:43 +0000

    copying local files with python

---
+++ copycat01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""JMolato
+   Copying files and folders"""
+
+# import additional code to complete our task
+import shutil
+import os
+
+def main():
+    # Move into a working Directory
+    os.chdir("/home/student/mycode/")
+
+    # Copy file a to b
+    shutil.copy("5g_research/sdn_network.txt", "5g_research/sdn_network.txt.copy")
+
+    # Copy the entire directorya to directoryb
+    shutil.copytree("5g_research/", "5g_research_backup/")
+
+# main guard
+if __name__ == "__main__":
+    main()
+
