b2192948f8be48f841a1af642759027cfbcd8a09
johnmmolato <93026051+johnmmolato@users.noreply.github.com>
2022-10-05 23:50:04 -0700

    major change to project

---
+++ class_project/database.py
@@ -0,0 +1,62 @@
+"""The database for the inventory system"""
+# import
+import sqlite3
+
+def connect():
+    """establish connection to the database"""
+    conn = sqlite3.connect("item.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS item (id INTEGER PRIMARY KEY, name text, qty real, price real, date text)")
+    conn.commit()
+    conn.close()
+
+
+def insert(name, price, qty, date):
+    """insert data to the database"""
+    conn = sqlite3.connect("item.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO item VALUES (NULL,?,?,?,?)", (name, price, qty, date))
+    conn.commit()
+    conn.close()
+
+
+def view():
+    """grab all inventory from the database"""
+    conn = sqlite3.connect("item.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM item")
+    views = cur.fetchall()
+    conn.close()
+    return views
+
+
+def search(name="", price="", qty="", date=""):
+    """grab provided user input"""
+    conn = sqlite3.connect("item.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM item WHERE name=? OR price=? OR qty=? OR date=?", (name, price, qty, date))
+    searches = cur.fetchall()
+    conn.close()
+    return searches
+
+
+def delete(id):
+    """delete the selected item"""
+    conn = sqlite3.connect("item.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM item WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+
+def update(id, name, price, qty, date):
+    """update the list"""
+    conn = sqlite3.connect("item.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE item SET name=?,price=?, qty=?, date=? WHERE id=?", (name, price, qty, date, id))
+    conn.commit()
+    conn.close()
+
+
+# connect as soon as main starts
+connect()
--- class_project/main.py
+++ class_project/main.py
@@ -1,76 +1,143 @@
-#!/usr/bin/env python3
-"""JMolato | Python class project
-   Inventory Management System"""
+"""John Molato | Class Project
+This is an inventory management system application
+with the functionality that allows user to search,
+create, update, delete"""
 
-# Import
+# imported libraries and files
 from tkinter import *
-from class_project import add_user
-from class_project import add_item
+from tkinter import messagebox
+from tkinter import ttk
+import database
 
 
-class Main:
+class Inventory:
     def __init__(self, master):
         self.master = master
+        self.widget()
+        self.list_all()
 
-        # mainframe
-        main_frame = Frame(self.master)
-        main_frame.pack()
-        # top frame
-        top_frame = Frame(main_frame, width=1200, height=90, bg="#c1f7ef", relief='ridge', borderwidth=2)
-        top_frame.pack(side=TOP, fill=X)
-        # center frame
-        center_frame = Frame(main_frame, width=1200, height=710, bg="#c1e7f7", relief='ridge', borderwidth=2)
-        center_frame.pack(side=TOP)
-        # add user button
-        self.iconuser = PhotoImage(file="icons/adduser.png")
-        self.btnuser = Button(top_frame, text="Add User", font="arial 14 bold", padx=20, command=self.add_user)
-        self.btnuser.configure(image=self.iconuser, compound=LEFT)
-        self.btnuser.pack(side=LEFT, padx=20)
-        # inventory button
-        self.iconsearch = PhotoImage(file="icons/search.png")
-        self.btnsearch = Button(top_frame, text="Inventory", font="arial 14 bold", padx=20, pady=6,
-                                command=self.add_inv)
-        self.btnsearch.configure(image=self.iconsearch, compound=LEFT)
-        self.btnsearch.pack(side=LEFT, padx=50)
-        # search bar
-        search_entry = Entry(top_frame, width=30, bd=4)
-        search_entry.pack(side=LEFT, padx=10)
-        search_button = Button(top_frame, text="Search", font="arial 14 bold", bg="#2488ff", width=10, height=2,
-                               command=self.search_inv)
-        search_button.pack(side=LEFT)
-        # inventory list
-        self.list_inv = Listbox(width=600, height=150, bd=10, font="arial 14 bold")
-        self.scroll_bar = Scrollbar(orient=VERTICAL)
-        #self.list_inv.grid(row=0, column=0, sticky=N)
-        self.scroll_bar.config(command=self.list_inv.yview())
-        self.list_inv.config(yscrollcommand=self.scroll_bar.set)
 
-    def add_user(self):
-        user_add = add_user.AddUser()
+    def list_all(self):
+        self.list_area.delete(0, END)
+        for item in database.view():
+            self.list_area.insert(END, item)
 
-    def add_inv(self):
-        inventory = add_item.AddItem()
 
-    def search_inv(self):
-        print("search inventory")
+    def search_item(self):
+        self.list_area.delete(0, END)
+        for item in database.search(self.name_text.get(), self.price_text.get(), self.qty_text.get(), self.purch_date_text.get()):
+            self.list_area.insert(END, item)
+        self.clear_field()
 
-    def search_bar(self):
-        print("")
 
-    def list_inventory(self):
-        print("Listing inventory")
+    def add_item(self):
+        name = self.name_text.get()
+        price = self.price_text.get()
+        qty = self.qty_text.get()
+        purch_date = self.purch_date_text.get()
+
+        if name and price and qty and purch_date != "":
+            try:
+                database.insert(name, price, qty, purch_date)
+                self.list_area.delete(0, END)
+                self.list_area.insert(END, (name, price, qty, purch_date))
+                self.clear_field()
+                messagebox.showinfo("Success!", "Item is succesfully added to the database", icon="info")
+            except:
+                messagebox.showerror("Failed to add to the database", icon="warning")
+        else:
+            messagebox.showerror("Fields are required", "Please fill in all fields", icon="warning")
+
+    def clear_field(self):
+        self.name_entry.delete(0, END)
+        self.price_entry.delete(0, END)
+        self.qty_entry.delete(0, END)
+        self.purch_date_entry.delete(0, END)
+
+
+    def get_selected_item(self, event):
+        try:
+            global selected_item
+            index = self.list_area.curselection()[0]
+            selected_item = self.list_area.get(index)
+            return selected_item
+        except IndexError:
+            pass
+
+
+    def delete_item(self):
+        try:
+            database.delete(selected_item[0])
+            self.list_all()
+        except:
+            pass
+
+    def update_item(self):
+        try:
+            database.update(selected_item[0], self.name_text.get(), self.price_text.get(), self.qty_text.get(), self.purch_date_text.get())
+            self.list_all()
+        except:
+            pass
+
+    def widget(self):
+        # name
+        self.name = Label(self.master, text="Name")
+        self.name.grid(row=0, column=0)
+        self.name_text = StringVar()
+        self.name_entry = Entry(self.master, textvariable=self.name_text)
+        self.name_entry.grid(row=0, column=1)
+
+        # price
+        self.price = Label(self.master, text="Price")
+        self.price.grid(row=0, column=2)
+        self.price_text = StringVar()
+        self.price_entry = Entry(self.master, textvariable=self.price_text)
+        self.price_entry.grid(row=0, column=3)
+
+        # quantity
+        self.qty = Label(self.master, text="Quantity")
+        self.qty.grid(row=1, column=0)
+        self.qty_text = StringVar()
+        self.qty_entry = Entry(self.master, textvariable=self.qty_text)
+        self.qty_entry.grid(row=1, column=1)
+
+        # purchase date
+        self.purch_date = Label(self.master, text="Purchase Date")
+        self.purch_date.grid(row=1, column=2)
+        self.purch_date_text = StringVar()
+        self.purch_date_entry = Entry(self.master, textvariable=self.purch_date_text)
+        self.purch_date_entry.grid(row=1, column=3)
+
+        # list area
+        self.list_area = Listbox(self.master, height=20, width=50)
+        self.list_area.grid(row=2, column=0, rowspan=6, columnspan=2)
+
+        # scrollbar
+        self.scrollbar = Scrollbar(self.master)
+        self.scrollbar.grid(row=2, column=2, rowspan=6)
+        self.list_area.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.configure(command=self.list_area.yview)
+        self.list_area.bind('<<ListboxSelect>>', self.get_selected_item)
+
+        # buttons
+        self.list_btn = Button(self.master, text="List all", fg="blue", bg="blue", relief="raised", width=10, command=self.list_all)
+        self.list_btn.grid(row=2, column=3)
+        self.search_btn = Button(self.master, text="Search", width=10, command=self.search_item)
+        self.search_btn.grid(row=3, column=3)
+        self.add_btn = Button(self.master, text="Add", width=10, command=self.add_item)
+        self.add_btn.grid(row=4, column=3)
+        self.update_btn = Button(self.master, text="Update", width=10, command=self.update_item)
+        self.update_btn.grid(row=5, column=3)
+        self.delete_btn = Button(self.master, text="Delete", width=10, command=self.delete_item)
+        self.delete_btn.grid(row=6, column=3)
 
 def main():
-    # create a new viewing window
     window = Tk()
-    app = Main(window)
-    window_title = "Inventory Management System"
-    # set title and viewing window
-    window.title(window_title)
-    window.geometry("1200x800")
-    # start window
+    app = Inventory(window)
+    window.title("Inventory Management System")
+    window.geometry("800x400")
+    window.configure(bg="#c1e7f7")
     window.mainloop()
-
 
 if __name__ == '__main__':
     main()
